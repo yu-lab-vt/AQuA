c960df68ab5731fddbb5af11c6b648f8822aa233
Yizhi Wang <freemanwyz@gmail.com>
2018-05-24 18:31:35 -0400

    More robust color map reading

--- src/+ui/+over/getColMap.m
+++ src/+ui/+over/getColMap.m
@@ -2,18 +2,32 @@ function getColMap(~,~,f)
    
     tb = readtable('./cfg/userColors.csv','Delimiter',',','ReadVariableNames',0,'ReadRowNames',1);         
     colNames = tb.Properties.RowNames;
+    tbc = table2cell(tb);
     
     % colorbrew
     nCol = numel(colNames);
     colVals = cell(nCol,1);
     for ii=1:nCol
-        v0 = tb{ii,1:end};
+        v0 = tbc(ii,1:end);
+        %v0 = tb{ii,1:end};
         idxGood = ~cellfun(@isempty,v0);
         nGood = sum(idxGood);
         if nGood>1
-            val0 = nan(nGood,3);
-            for jj=1:nGood
-                val0(jj,:) = eval(['[',v0{jj},']']);
+            val0 = nan(1,3);
+            nxx = 1;
+            for jj=1:numel(v0)
+                if isempty(v0{jj})
+                    break
+                end
+                try
+                    tmp = eval(['[',v0{jj},']']);
+                    if numel(tmp)==3
+                        val0(nxx,:) = tmp;
+                        nxx = nxx + 1;
+                    end
+                catch
+                    %fprintf('Some error in userColors.csv: %d\n',ii)
+                end
             end
             colVals{ii} = val0;
         else % search in colorbrew
