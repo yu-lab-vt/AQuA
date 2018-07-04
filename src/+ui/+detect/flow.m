4684a46cb3eeeff9bd97a6cb9f560a265ada3d8f
Yizhi Wang <freemanwyz@gmail.com>
2018-07-04 16:35:51 -0400

    debug for feature table error

--- src/+ui/+detect/flow.m
+++ src/+ui/+detect/flow.m
@@ -1,71 +1,71 @@
 function flow(~,evtDat,f,op)
-    
-    fh = guidata(f);
-    nTabTot = numel(fh.deOutTab.TabEnables);
-    ixTab = fh.deOutTab.Selection;
-    
-    % controls
-    if strcmp(op,'chg')
-        ixTab = evtDat.NewValue;
-        fh.deOutBack.Visible = 'on';
-        fh.deOutNext.Visible = 'on';
+
+fh = guidata(f);
+nTabTot = numel(fh.deOutTab.TabEnables);
+ixTab = fh.deOutTab.Selection;
+
+% controls
+if strcmp(op,'chg')
+    ixTab = evtDat.NewValue;
+    fh.deOutBack.Visible = 'on';
+    fh.deOutNext.Visible = 'on';
+    fh.deOutNext.Enable = 'on';
+    fh.deOutRun.String = 'Run';
+    switch ixTab
+        case 1
+            fh.deOutBack.Visible = 'off';
+        case 4
+            fh.deOutRun.String = 'Apply';
+        case 5
+            fh.deOutRun.String = 'Apply';
+        case 7
+            fh.deOutRun.String = 'Extract';
+            fh.deOutNext.Visible = 'off';
+    end
+    if ixTab<nTabTot
+        if strcmp(fh.deOutTab.TabEnables{ixTab+1},'off')
+            fh.deOutNext.Enable = 'off';
+        end
+    end
+end
+
+% go to previous step
+if strcmp(op,'back')
+    if ixTab>1
+        fh.deOutTab.Selection = ixTab-1;
+    end
+end
+
+% run current step
+if strcmp(op,'run')
+    switch ixTab
+        case 1
+            ui.detect.actRun([],[],f);
+        case 2
+            ui.detect.phaseRun([],[],f);
+        case 3
+            ui.detect.evtRun([],[],f);
+        case 4
+            ui.detect.zsRun([],[],f);
+        case 5
+            ui.detect.mergeRun([],[],f);
+        case 6
+            ui.detect.evtReRun([],[],f);
+        case 7
+            ui.detect.feaRun([],[],f);
+    end
+    if ixTab<nTabTot
         fh.deOutNext.Enable = 'on';
-        fh.deOutRun.String = 'Run';
-        switch ixTab
-            case 1
-                fh.deOutBack.Visible = 'off';
-            case 4
-                fh.deOutRun.String = 'Apply';
-            case 5
-                fh.deOutRun.String = 'Apply';
-            case 7
-                fh.deOutRun.String = 'Extract';
-                fh.deOutNext.Visible = 'off';
-        end
-        if ixTab<nTabTot
-            if strcmp(fh.deOutTab.TabEnables{ixTab+1},'off')
-                fh.deOutNext.Enable = 'off';
-            end
-        end
     end
-    
-    % go to previous step
-    if strcmp(op,'back')        
-        if ixTab>1
-            fh.deOutTab.Selection = ixTab-1;
-        end        
+end
+
+% go to next step
+if strcmp(op,'next')
+    if ixTab<nTabTot
+        fh.deOutTab.Selection = ixTab+1;
+        fh.deOutTab.TabEnables{ixTab+1} = 'on';
     end
-    
-    % run current step
-    if strcmp(op,'run')
-        switch ixTab
-            case 1
-                ui.detect.actRun([],[],f);
-            case 2
-                ui.detect.phaseRun([],[],f);
-            case 3
-                ui.detect.evtRun([],[],f);
-            case 4
-                ui.detect.zsRun([],[],f);
-            case 5
-                ui.detect.mergeRun([],[],f);
-            case 6
-                ui.detect.evtReRun([],[],f);
-            case 7
-                ui.detect.feaRun([],[],f);
-        end
-        if ixTab<nTabTot
-            fh.deOutNext.Enable = 'on';
-        end
-    end
-        
-    % go to next step
-    if strcmp(op,'next')
-        if ixTab<nTabTot
-            fh.deOutTab.Selection = ixTab+1;
-            fh.deOutTab.TabEnables{ixTab+1} = 'on';
-        end
-    end    
+end
 end
 
 
---
+++ src/+ui/+detect/saveExpDbg.m
@@ -0,0 +1,65 @@
+function res = saveExpDbg(~,~,f)
+    % saveExp save experiment (and export results)
+    
+    fts = getappdata(f,'fts');
+    
+    % gather results
+    
+    vSave0 = {...  % basic variables for results analysis
+        'opts','scl','btSt','ov','bd','datOrg','evt','fts','dffMat','dMat',...
+        'riseLst','featureTable','userFeatures'...
+        };
+    vSave1 = {...  % extra variables for event detection
+        'arLst','lmLoc','svLst','riseX','riseLstAll','evtLstAll','ftsLstAll',...
+        'dffMatAll','datRAll','evtLstFilterZ','dffMatFilterZ','tBeginFilterZ',...
+        'riseLstFilterZ','evtLstMerge','dF'...
+        };
+    vSave = [vSave0,vSave1];
+    
+    res = [];
+    for ii=1:numel(vSave)
+        v0 = vSave{ii};
+        res.(v0) = getappdata(f,v0);
+    end
+    
+    % filter features and curves
+    try
+        ov = getappdata(f,'ov');
+        ov0 = ov('Events');
+        xSel = ov0.sel;
+        
+        res.ftsFilter = util.filterFields(fts,xSel);
+        res.evtFilter = res.evt(xSel);
+        res.dffMatFilter = res.dffMat(xSel,:,:);
+        if ~isempty(res.dMat)
+            res.dMatFilter = res.dMat(xSel,:,:);
+        end
+        if ~isempty(res.riseLst)  % rising map is for super events
+            res.riseLstFilter = res.riseLst(xSel);
+        end
+        res.evtSelectedList = find(xSel>0);
+    catch        
+    end
+    
+    % save raw movie with 8 or 16 bits to save space
+    res.opts.bitNum = 16;
+    res.maxVal = nanmax(res.datOrg(:));
+    res.datOrg = res.datOrg/res.maxVal;
+    dat1 = res.datOrg*(2^res.opts.bitNum-1);
+    res.datOrg = uint16(dat1);
+    
+    res.stg.post = 1;
+    res.stg.detect = 1;
+    res.dbg = 1;
+    
+    save('debug.mat','res','-v7.3');
+    
+    
+end
+
+
+
+
+
+
+
