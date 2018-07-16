8cf03b63e3b694890f064aab9fca7d61d43f701f
Yizhi Wang <freemanwyz@gmail.com>
2018-07-16 15:19:41 -0400

    add multiple events to favorite list

--- src/+ui/+evt/addOne.m
+++ src/+ui/+evt/addOne.m
@@ -1,18 +1,20 @@
 function addOne(~,~,f)
+    % add one or multiple events to favorite list
     
     fh = guidata(f);
     btSt = getappdata(f,'btSt');
     
     try
-        evtNow = str2double(fh.toolsAddEvt.String);        
+        % evtNow = str2double(fh.toolsAddEvt.String);        
+        evtNow = eval(fh.toolsAddEvt.String);        
         lst = union(evtNow,btSt.evtMngrMsk);        
         btSt.evtMngrMsk = lst;
         setappdata(f,'btSt',btSt);
         ui.evt.evtMngrRefresh([],[],f);
         fts = getappdata(f,'fts');
         
-        n0 = fts.curve.tBegin(evtNow);
-        n1 = fts.curve.tEnd(evtNow);
+        n0 = fts.curve.tBegin(evtNow(1));
+        n1 = fts.curve.tEnd(evtNow(1));
         n = round((n0+n1)/2);        
         ui.movStep(f,n,[],1);
     catch
