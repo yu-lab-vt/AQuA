a4e5c2fd107909d654f7ab94b1f9f3972fd6db6f
Yizhi Wang <freemanwyz@gmail.com>
2018-07-16 21:50:13 -0400

    user specify suffix in output

--- src/+ui/+proj/getOutputFolder.m
+++ src/+ui/+proj/getOutputFolder.m
@@ -1,7 +1,20 @@
 function getOutputFolder(~,~,f)
     
     opts = getappdata(f,'opts');
-    file0 = [opts.fileName,'_aqua'];
+    
+    % SP, 18.07.16
+    definput = {'_AQuA'};
+    selname = inputdlg('Type desired suffix for output files name:',...
+        'Output files',[1 75],definput);
+    
+    selname = char(selname);
+    if isempty(selname)
+        selname = '_AQuA';
+    end
+    file0 = [opts.fileName,selname];
+    clear definput selname
+    
+    %file0 = [opts.fileName,'_AQuA']; SP, 18.07.16
     selpath = uigetdir('.','Choose output folder');
     path0 = [selpath,filesep,opts.fileName];
     if ~isnumeric(selpath)
