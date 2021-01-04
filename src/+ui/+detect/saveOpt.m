e1403920bfb772ce1f77363173280716b15eed04
Xuelong <mixl18@vt.edu>
2021-01-04 12:19:59 -0900

    fix bug for saving opts

--- src/+ui/+detect/saveOpt.m
+++ src/+ui/+detect/saveOpt.m
@@ -1,6 +1,23 @@
 function saveOpt(~,~,f)
     
     opts = getappdata(f,'opts');
+    % should update new changing
+    fh = guidata(f);
+    opts.thrARScl = str2double(fh.thrArScl.String);
+    opts.smoXY = str2double(fh.smoXY.String);
+    opts.minSize = str2double(fh.minSize.String);
+    opts.thrTWScl = str2double(fh.thrTWScl.String);
+    opts.thrExtZ = str2double(fh.thrExtZ.String);
+    opts.cRise = str2double(fh.cRise.String);
+    opts.cDelay = str2double(fh.cDelay.String);
+    opts.gtwSmo = str2double(fh.gtwSmo.String);
+    opts.zThr = str2double(fh.zThr.String);
+    opts.ignoreMerge = fh.ignoreMerge.Value==1;
+    opts.mergeEventDiscon = str2double(fh.mergeEventDiscon.String);
+    opts.mergeEventCorr = str2double(fh.mergeEventCorr.String);
+    opts.mergeEventMaxTimeDif = str2double(fh.mergeEventMaxTimeDif.String);
+    opts.extendEvtRe = fh.extendEvtRe.Value==1;
+    opts.ignoreTau = fh.ignoreTau.Value==1;
     
     % SP, 18.07.16
     definput = {'_Opt.csv'};
