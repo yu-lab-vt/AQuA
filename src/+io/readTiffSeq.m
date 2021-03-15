86ded6774f92804582ed6d1710ec0369e58dec24
Xuelong <mixl18@vt.edu>
2021-03-15 10:51:51 -0400

    if data is color image, will convert to gray image

--- src/+io/readTiffSeq.m
+++ src/+io/readTiffSeq.m
@@ -9,10 +9,17 @@ info = imfinfo(fName);
 maxVal = 2^info(1).BitDepth-1;
 nFrames = numel(info);
 oneFrame = imread(fName, 1);
-[nRow,nCol] = size(oneFrame);
+% change
+nRow = info(1).Height;
+nCol = info(1).Width;
+nChannel = info(1).SamplesPerPixel;
+% [nRow,nCol] = size(oneFrame);
 img = zeros(nRow, nCol, nFrames, 'single');
 for k = 1:nFrames
     oneFrame = imread(fName, k);
+    if(nChannel>1)
+        oneFrame = mean(oneFrame,3);
+    end
     if rescaleImg
         img(:,:,k) = single(oneFrame)/maxVal;
     else
